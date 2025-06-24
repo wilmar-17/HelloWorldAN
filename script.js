4000ece5d4ec757743b094120a57c8a525e050ee
wilmar-17 <wilmar.armelin@escola.pr.gov.br>
2025-06-23 21:40:08 -0300

    função

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function inserirNome(){
-let nomeUsuario = prompt("Qual o seu nome")
-let elemento = document.querySelector("#nomeUsuario");
+let nomeUsuario = prompt("Qual o seu nome");
+let elemento = document.querySelector("#nome-usuario");
 elemento.textContent = nomeUsuario;
 }
 inserirNome();
