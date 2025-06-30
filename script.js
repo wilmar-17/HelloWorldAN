cab9e0283f2c33886ec8510a5c12cb7d6c0400a4
wilmar-17 <wilmar.armelin@escola.pr.gov.br>
2025-06-30 18:43:24 -0300

    console

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ let nomeUsuario = prompt("Qual o seu nome");
 let elemento = document.querySelector("#nome-usuario");
 elemento.textContent = nomeUsuario;
 }
-inserirNome();
+inserirNome()
+
+let linguagens = ["Javascript", "Python", "C"];
+console.log(linguagens[0]);
+console.log(linguagens[1]);
+console.log(linguagens[2]);
