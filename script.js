a99675f39b441c28c5c1c8afd031a1b80cc115f3
wilmar-17 <wilmar.armelin@escola.pr.gov.br>
2025-06-30 21:24:55 -0300

    console

--- script.js
+++ script.js
@@ -6,5 +6,12 @@ elemento.textContent = nomeUsuario;
 inserirNome()
 
 let linguagens = ["Javascript", "Python", "C"];
+console.log(linguagens[0]) // “JavaScript”
+console.log(linguagens[1]) // “Python”
+console.log(linguagens[2]) // “C”
 const item = document.querySelector("#lista");
 item.textContent = linguagens[0];
+
+let estudantes = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo"];
+const item = document.querySelector("#alunos");
+item.textContent = estudantes[0];
